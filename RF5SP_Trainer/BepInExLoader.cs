3fba32f92e421be1019c9649468b7ea7e73f8853
Bảo <44154735+baoenzo@users.noreply.github.com>
2022-07-20 10:33:50 +0700

    Add project files.

---
+++ RF5SP_Trainer/BepInExLoader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+namespace RF5SP_Trainer
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "RF5Trainer",
+            AUTHOR = "baoenzo",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("[Trainer] Registering TrainerComponent in Il2Cpp");
+
+            try
+            {
+                // Register our custom Types in Il2Cpp
+                ClassInjector.RegisterTypeInIl2Cpp<TrainerComponent>();
+
+                var go = new GameObject("TrainerObject");
+                go.AddComponent<TrainerComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("[Trainer] FAILED to Register Il2Cpp Type: TrainerComponent!");
+            }
+
+            try
+            {
+                var harmony = new Harmony("baoenzo.trainer.il2cpp");
+
+                // Our Primary Unity Event Hooks 
+
+                // Awake
+                //var originalAwake = AccessTools.Method(typeof(SOME_GAME_TYPE), "Awake"); // Change Type!
+                //log.LogMessage("[Trainer] Harmony - Original Method: " + originalAwake.DeclaringType.Name + "." + originalAwake.Name);
+                //var postAwake = AccessTools.Method(typeof(TrainerComponent), "Awake");
+                //log.LogMessage("[Trainer] Harmony - Postfix Method: " + postAwake.DeclaringType.Name + "." + postAwake.Name);
+                //harmony.Patch(originalAwake, postfix: new HarmonyMethod(postAwake));
+
+                //// Start
+                //var originalStart = AccessTools.Method(typeof(SOME_GAME_TYPE), "Start"); // Change Type!
+                //log.LogMessage("[Trainer] Harmony - Original Method: " + originalStart.DeclaringType.Name + "." + originalStart.Name);
+                //var postStart = AccessTools.Method(typeof(TrainerComponent), "Start");
+                //log.LogMessage("[Trainer] Harmony - Postfix Method: " + postStart.DeclaringType.Name + "." + postStart.Name);
+                //harmony.Patch(originalStart, postfix: new HarmonyMethod(postStart));
+
+                // Update
+                var originalUpdate = AccessTools.Method(typeof(UnityEngine.UI.CanvasScaler), "Update");
+                log.LogMessage("[Trainer] Harmony - Original Method: " + originalUpdate.DeclaringType.Name + "." + originalUpdate.Name);
+                var postUpdate = AccessTools.Method(typeof(TrainerComponent), "Update");
+                log.LogMessage("[Trainer] Harmony - Postfix Method: " + postUpdate.DeclaringType.Name + "." + postUpdate.Name);
+                harmony.Patch(originalUpdate, postfix: new HarmonyMethod(postUpdate));
+                log.LogMessage("[Trainer] Harmony - Runtime Patch's Applied");
+                
+
+            }
+            catch
+            {
+                log.LogError("[Trainer] Harmony - FAILED to Apply Patch's!");
+            }
+           
+        }
+    }
+}
---
+++ RF5SP_Trainer/TrainerComponent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Input = BepInEx.IL2CPP.UnityEngine.Input;
+
+namespace RF5SP_Trainer
+{
+    public class TrainerComponent : MonoBehaviour
+    {
+        public TrainerComponent(IntPtr ptr) : base(ptr)
+        {
+            BepInExLoader.log.LogMessage("[Trainer] Entered Constructor");
+        }
+
+        // Harmony Patch's must be static!
+        [HarmonyPostfix]
+        public static void Awake()
+        {
+            BepInExLoader.log.LogMessage("[Trainer] I'm Awake!");
+        }
+
+        [HarmonyPostfix]
+        public static void Start()
+        {
+            BepInExLoader.log.LogMessage("[Trainer] I'm Starting Up...");
+        }
+
+        [HarmonyPostfix]
+        public static void Update()
+        {
+            if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F1) && Event.current.type == EventType.KeyDown)
+
+            {
+                SV.AddPlayerGold(10000);
+                BepInExLoader.log.LogMessage("Add 100000 Gold !!!");
+                Event.current.Use();
+            }
+            else if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F2) && Event.current.type == EventType.KeyDown)
+            {
+                SV.AddLumber(100);
+                SV.AddStone(100);
+                BepInExLoader.log.LogMessage("Add 100 lumber and stones !!!");
+                Event.current.Use();
+
+            }
+            else if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F3) && Event.current.type == EventType.KeyDown)
+            {
+                SkillManager.RandomSkillLvUp();
+                BepInExLoader.log.LogMessage("Random LV Up Skills");
+                Event.current.Use();
+            }
+            else if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F4) && Event.current.type == EventType.KeyDown)
+            {
+                SV.AddSeedPoint(10000);
+                BepInExLoader.log.LogMessage("Add 10000 SEED Points ");
+                Event.current.Use();
+            }
+            
+        }
+    }
+}
